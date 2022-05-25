9ca2d7c8103b7e3fc1cf058e4bd01f288381eb0c
DanielTrGa <danieltriguerogalvez@hotmail.com>
2022-05-25 13:14:53 +0200

    jDoc y nuevo metodo de 1 digito

--- src/sumador/ASumar.java
+++ src/sumador/ASumar.java
@@ -2,4 +2,21 @@ package sumador;
 
 public class ASumar {
 
+	/**
+	 * @author Daniel
+	 * @version 1.0
+	 * @param cad. Se le pasa el numero que queramos usar
+	 * @return devuelve el mismo numero si tiene una longitud de 1. Y null si tiene una longitud de 2 o más
+	 *
+	 */
+	
+	public String sumativo(String cad) {
+		String numero = cad;
+        if (numero.length() == 1) {
+            return (numero + " = " + numero);
+        }
+        return null;
+        
+	}
+
 }
--- test/sumador/TestASumar.java
+++ test/sumador/TestASumar.java
@@ -4,11 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+
 class TestASumar {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	
+	
+	 @Test
+	    public void testUnaCifra() {
+	        ASumar sumi = new ASumar();
+	        String resultado = sumi.sumativo("5");
+	        assertEquals("5 = 5",resultado );
+	    }
+	 
+	 
 
 }
